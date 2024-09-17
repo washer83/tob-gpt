cd73857e1269ca5c7951e98d414b6a5202dc5bde
washer83 <washer8394@gmail.com>
2024-09-17 01:50:44 -0400

    Move attack types out of player class for cleanliness.

---
+++ AttackTypes.py
@@ -0,0 +1,12 @@
+class AttackStyle:
+    ACCURATE = "Accurate"
+    AGGRESSIVE = "Aggressive"
+    DEFENSIVE = "Defensive"
+    CONTROLLED = "Controlled"
+    RAPID = "Rapid"
+    LONGRANGE = "Longrange"
+
+class AttackType:
+    MELEE = "Melee"
+    RANGED = "Ranged"
+    MAGIC = "Magic"
