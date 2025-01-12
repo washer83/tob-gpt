a860dfd8ed8f8c92874c971ddc38690ecbf63d63
washer <washer8394@gmail.com>
2025-01-11 19:17:51 -0500

    Update misc_calcs.py
    Add a function to convert from tick output to time in MM:SS format.

--- util/misc_calcs.py
+++ util/misc_calcs.py
@@ -1,5 +1,25 @@
 import random
 
+def ticks_to_time(ticks):
+    """
+    Converts game ticks to MM:SS format.
+    
+    :param ticks: int/list of game ticks
+    :return: String (or list) in MM:SS format
+    """
+    def convert(t):
+        total_seconds = t * 0.6
+        minutes = int(total_seconds // 60)
+        seconds = int(total_seconds % 60)
+        return f"{minutes:02}:{seconds:02}"
+    
+    if isinstance(ticks, list):
+        return [convert(t) for t in ticks]
+    if isinstance(ticks, int):
+        return convert(ticks)
+    else:
+        raise ValueError("Input must be an int or list.")
+
 def calculate_purple_crab_hit(player) -> bool:
     player_attack_roll = player.calculate_attack_roll()
     purple_crab_def_roll = 3776
